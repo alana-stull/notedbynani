7d498d653be8824e30d4f64e1d551a14e34e0c9f
alana-stull <stull.alana@gmail.com>
2026-04-06 16:04:30 -0400

    made logo bigger

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -41,7 +41,7 @@ export default function App() {
               padding: 0
             }}
           >
-            <img src={logoImage} alt="Noted by Nani" style={{ height: '36px', display: 'block' }} />
+            <img src={logoImage} alt="Noted by Nani" style={{ height: '64px', display: 'block' }} />
           </button>
           <div style={{ display: 'flex', gap: '28px', alignItems: 'center' }}>
             <button
